69f7ff85c995e587cc3d7499776e1361d13d85a1
HoolaBoola <ale@hoolaboola.fi>
2021-11-19 17:39:29 +0200

    kurssitiedot ex2.5

---
+++ part2/kurssitiedot/src/components/Total.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Total = ({ parts }) => {
+  const total = parts.reduce( (a, b) => a + b.exercises, 0);
+  return (
+    <p>
+        <b> Total of {total} exercises 
+        </b>
+    </p>
+  )
+}
+
+export default Total
