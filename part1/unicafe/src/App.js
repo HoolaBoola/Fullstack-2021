a915fe1751701ef5f83c4f1fc1cb7ac32e9af80e
HoolaBoola <ale@hoolaboola.fi>
2021-11-19 15:13:10 +0200

    unicafe works ex1.11

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <div>
+        <h1>give feedback</h1>
+          
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+        <Statistics good={good} bad={bad} neutral={neutral} />
+      </div>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+      <div>
+        <h1>statistics</h1>
+        <table>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+          <StatisticLine text="positive" value={good / (good + neutral + bad)} />
+        </table>
+      </div>
+    )  
+}
+
+const StatisticLine = (props) => (
+  <tr>
+    <td>
+      {props.text} {props.value}
+    </td>
+  </tr>
+)
+export default App
