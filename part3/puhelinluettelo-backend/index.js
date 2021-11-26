023ad25218527360a653853a63eba124262cbfd3
HoolaBoola <ale@hoolaboola.fi>
2021-11-26 21:59:27 +0200

    uibuild

--- part3/puhelinluettelo-backend/index.js
+++ part3/puhelinluettelo-backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('content', (req, res) => JSON.stringify(req.body));
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [  
